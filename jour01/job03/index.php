e60c75acfd65c91fbde29bd7225a7e58172be598
Mickael-AY <mickaelayilan@hotmail.com>
2025-09-16 11:51:56 +0200

    job03 fini

---
+++ jour01/job03/index.php
@@ -0,0 +1,26 @@
+<?php
+// booléen 
+$var1=false;
+// Entier
+$var2=7;
+// Nombre à virgule flottante
+$var3=7.2;
+// Chaîne de caractères
+$var4="je crée une chaîne de caractères";
+ 
+ 
+//TABLEAU PHP
+ 
+echo "<table>
+ 
+<tr><th> Nom </th><th> Type </th><th> Valeur </th></tr>";
+ 
+echo "<tr><td> Or </td><td> +++ </td><td> 1000 </td></tr>";
+ 
+echo "<tr><td> Argent </td><td> ++ </td><td> 500 </td></tr>";
+ 
+echo "<tr><td> Cruivre </td><td> + </td><td> 10 </td></tr>";
+ 
+echo "</table>";
+ 
+?>
